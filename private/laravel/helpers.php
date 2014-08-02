a45ced796cc7d6757b917f1e6bc33f8972eb4632
eplightning <eplightning@outlook.com>
2014-08-03 00:14:24 +0200

    Usunięto Laravel/Section ,z którego i tak nie korzystamy a powoduje problemy na nowszych wersjach PHP

--- private/laravel/helpers.php
+++ private/laravel/helpers.php
@@ -562,7 +562,7 @@ function render_each($partial, array $data, $iterator, $empty = 'raw|')
  */
 function yield_content($section)
 {
-	return Laravel\Section::yield_content($section);
+	return '';
 }
 
 /**
