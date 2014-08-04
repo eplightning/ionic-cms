60531e542c8d2d1e715cb4247433e82950d7507e
eplightning <eplightning@outlook.com>
2014-08-04 21:21:39 +0200

    Aktualizacja SwiftMailera

--- private/application/vendor/SwiftMailer/classes/Swift/Plugins/ThrottlerPlugin.php
+++ private/application/vendor/SwiftMailer/classes/Swift/Plugins/ThrottlerPlugin.php
@@ -2,168 +2,203 @@
 
 /*
  * This file is part of SwiftMailer.
-* (c) 2004-2009 Chris Corbyn
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * (c) 2004-2009 Chris Corbyn
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
  * Throttles the rate at which emails are sent.
- * @package Swift
+ *
+ * @package    Swift
  * @subpackage Plugins
- * @author Chris Corbyn
+ * @author     Chris Corbyn
  */
 class Swift_Plugins_ThrottlerPlugin extends Swift_Plugins_BandwidthMonitorPlugin implements Swift_Plugins_Sleeper, Swift_Plugins_Timer
 {
-	/** Flag for throttling in bytes per minute */
-	const BYTES_PER_MINUTE = 0x01;
+    /** Flag for throttling in bytes per minute */
+    const BYTES_PER_MINUTE = 0x01;
 
-	/** Flag for throttling in emails per minute */
-	const MESSAGES_PER_MINUTE = 0x10;
+    /** Flag for throttling in emails per second (Amazon SES) */
+    const MESSAGES_PER_SECOND = 0x11;
 
-	/**
-	 * The Sleeper instance for sleeping.
-	 * @var Swift_Plugins_Sleeper
-	 * @access private
-	 */
-	private $_sleeper;
+    /** Flag for throttling in emails per minute */
+    const MESSAGES_PER_MINUTE = 0x10;
 
-	/**
-	 * The Timer instance which provides the timestamp.
-	 * @var Swift_Plugins_Timer
-	 * @access private
-	 */
-	private $_timer;
+    /**
+     * The Sleeper instance for sleeping.
+     *
+     * @var Swift_Plugins_Sleeper
+     */
+    private $_sleeper;
 
-	/**
-	 * The time at which the first email was sent.
-	 * @var int
-	 * @access private
-	 */
-	private $_start;
+    /**
+     * The Timer instance which provides the timestamp.
+     *
+     * @var Swift_Plugins_Timer
+     */
+    private $_timer;
 
-	/**
-	 * The rate at which messages should be sent.
-	 * @var int
-	 * @access private
-	 */
-	private $_rate;
+    /**
+     * The time at which the first email was sent.
+     *
+     * @var int
+     */
+    private $_start;
 
-	/**
-	 * The mode for throttling.
-	 * This is {@link BYTES_PER_MINUTE} or {@link MESSAGES_PER_MINUTE}
-	 * @var int
-	 * @access private
-	 */
-	private $_mode;
+    /**
+     * The rate at which messages should be sent.
+     *
+     * @var int
+     */
+    private $_rate;
 
-	/**
-	 * An internal counter of the number of messages sent.
-	 * @var int
-	 * @access private
-	 */
-	private $_messages = 0;
+    /**
+     * The mode for throttling.
+     *
+     * This is {@link BYTES_PER_MINUTE} or {@link MESSAGES_PER_MINUTE}
+     *
+     * @var int
+     */
+    private $_mode;
 
-	/**
-	 * Create a new ThrottlerPlugin.
-	 * @param int                   $rate
-	 * @param int                   $mode,   defaults to {@link BYTES_PER_MINUTE}
-	 * @param Swift_Plugins_Sleeper $sleeper (only needed in testing)
-	 * @param Swift_Plugins_Timer   $timer   (only needed in testing)
-	 */
-	public function __construct($rate, $mode = self::BYTES_PER_MINUTE, Swift_Plugins_Sleeper $sleeper = null, Swift_Plugins_Timer $timer = null)
-	{
-		$this->_rate = $rate;
-		$this->_mode = $mode;
-		$this->_sleeper = $sleeper;
-		$this->_timer = $timer;
-	}
+    /**
+     * An internal counter of the number of messages sent.
+     *
+     * @var int
+     */
+    private $_messages = 0;
 
-	/**
-	 * Invoked immediately before the Message is sent.
-	 * @param Swift_Events_SendEvent $evt
-	 */
-	public function beforeSendPerformed(Swift_Events_SendEvent $evt)
-	{
-		$time = $this->getTimestamp();
-		if (!isset($this->_start)) {
-			$this->_start = $time;
-		}
-		$duration = $time - $this->_start;
+    /**
+     * Create a new ThrottlerPlugin.
+     *
+     * @param int                   $rate
+     * @param int                   $mode,   defaults to {@link BYTES_PER_MINUTE}
+     * @param Swift_Plugins_Sleeper $sleeper (only needed in testing)
+     * @param Swift_Plugins_Timer   $timer   (only needed in testing)
+     */
+    public function __construct($rate, $mode = self::BYTES_PER_MINUTE, Swift_Plugins_Sleeper $sleeper = null, Swift_Plugins_Timer $timer = null)
+    {
+        $this->_rate = $rate;
+        $this->_mode = $mode;
+        $this->_sleeper = $sleeper;
+        $this->_timer = $timer;
+    }
 
-		if (self::BYTES_PER_MINUTE == $this->_mode) {
-			$sleep = $this->_throttleBytesPerMinute($duration);
-		} else {
-			$sleep = $this->_throttleMessagesPerMinute($duration);
-		}
+    /**
+     * Invoked immediately before the Message is sent.
+     *
+     * @param Swift_Events_SendEvent $evt
+     */
+    public function beforeSendPerformed(Swift_Events_SendEvent $evt)
+    {
+        $time = $this->getTimestamp();
+        if (!isset($this->_start)) {
+            $this->_start = $time;
+        }
+        $duration = $time - $this->_start;
 
-		if ($sleep > 0) {
-			$this->sleep($sleep);
-		}
-	}
+        switch($this->_mode) {
+            case self::BYTES_PER_MINUTE :
+                $sleep = $this->_throttleBytesPerMinute($duration);
+                break;
+            case self::MESSAGES_PER_SECOND :
+                $sleep = $this->_throttleMessagesPerSecond($duration);
+                break;
+            case self::MESSAGES_PER_MINUTE :
+                $sleep = $this->_throttleMessagesPerMinute($duration);
+                break;
+            default :
+                $sleep = 0;
+                break;
+        }
 
-	/**
-	 * Invoked when a Message is sent.
-	 * @param Swift_Events_SendEvent $evt
-	 */
-	public function sendPerformed(Swift_Events_SendEvent $evt)
-	{
-		parent::sendPerformed($evt);
-		++$this->_messages;
-	}
+        if ($sleep > 0) {
+            $this->sleep($sleep);
+        }
+    }
 
-	/**
-	 * Sleep for $seconds.
-	 * @param int $seconds
-	 */
-	public function sleep($seconds)
-	{
-		if (isset($this->_sleeper)) {
-			$this->_sleeper->sleep($seconds);
-		} else {
-			sleep($seconds);
-		}
-	}
+    /**
+     * Invoked when a Message is sent.
+     *
+     * @param Swift_Events_SendEvent $evt
+     */
+    public function sendPerformed(Swift_Events_SendEvent $evt)
+    {
+        parent::sendPerformed($evt);
+        ++$this->_messages;
+    }
 
-	/**
-	 * Get the current UNIX timestamp
-	 * @return int
-	 */
-	public function getTimestamp()
-	{
-		if (isset($this->_timer)) {
-			return $this->_timer->getTimestamp();
-		} else {
-			return time();
-		}
-	}
+    /**
+     * Sleep for $seconds.
+     *
+     * @param int     $seconds
+     */
+    public function sleep($seconds)
+    {
+        if (isset($this->_sleeper)) {
+            $this->_sleeper->sleep($seconds);
+        } else {
+            sleep($seconds);
+        }
+    }
 
-	// -- Private methods
+    /**
+     * Get the current UNIX timestamp.
+     *
+     * @return int
+     */
+    public function getTimestamp()
+    {
+        if (isset($this->_timer)) {
+            return $this->_timer->getTimestamp();
+        } else {
+            return time();
+        }
+    }
 
-	/**
-	 * Get a number of seconds to sleep for.
-	 * @param  int $timePassed
-	 * @return int
-	 * @access private
-	 */
-	private function _throttleBytesPerMinute($timePassed)
-	{
-		$expectedDuration = $this->getBytesOut() / ($this->_rate / 60);
+    // -- Private methods
 
-		return (int) ceil($expectedDuration - $timePassed);
-	}
+    /**
+     * Get a number of seconds to sleep for.
+     *
+     * @param int     $timePassed
+     *
+     * @return int
+     */
+    private function _throttleBytesPerMinute($timePassed)
+    {
+        $expectedDuration = $this->getBytesOut() / ($this->_rate / 60);
 
-	/**
-	 * Get a number of seconds to sleep for.
-	 * @param  int $timePassed
-	 * @return int
-	 * @access private
-	 */
-	private function _throttleMessagesPerMinute($timePassed)
-	{
-		$expectedDuration = $this->_messages / ($this->_rate / 60);
+        return (int) ceil($expectedDuration - $timePassed);
+    }
 
-		return (int) ceil($expectedDuration - $timePassed);
-	}
+    /**
+     * Get a number of seconds to sleep for.
+     *
+     * @param int $timePassed
+     *
+     * @return int
+     */
+    private function _throttleMessagesPerSecond($timePassed)
+    {
+        $expectedDuration = $this->_messages / ($this->_rate);
+
+        return (int) ceil($expectedDuration - $timePassed);
+    }
+
+    /**
+     * Get a number of seconds to sleep for.
+     *
+     * @param int     $timePassed
+     *
+     * @return int
+     */
+    private function _throttleMessagesPerMinute($timePassed)
+    {
+        $expectedDuration = $this->_messages / ($this->_rate / 60);
+
+        return (int) ceil($expectedDuration - $timePassed);
+    }
 }
